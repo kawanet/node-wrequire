b7e1c0f9c453073eefa9c4ea41da0ea6243e5052
kawanet <u-suke@kawa.net>
2013-06-11 18:11:38 +0900

    first commit

---
+++ wrequire.js
@@ -0,0 +1,15 @@
+/*! wrequire.js */
+
+var _window;
+
+try {
+  _window = window; // running on browser
+} catch (err) {
+  // running on node.js
+}
+
+_window = _window || {};
+
+module.exports = function(key, path) {
+  return _window[key] || require(path);
+};
