6120d4f1b01d8d98426bdaa562d65e4b5905a9b4
kawanet <u-suke@kawa.net>
2013-12-07 18:55:09 +0900

    0.0.5 check path

--- wrequire.js
+++ wrequire.js
@@ -3,9 +3,9 @@
 module.exports = wrequire;
 
 function wrequire(key, path) {
-  if (typeof window !== 'undefined' && window[key]) {
+  if (typeof window !== 'undefined' && typeof window[key] !== 'undefined') {
     return window[key]; // running on browser
-  } else if (typeof require !== 'undefined') {
+  } else if (typeof require !== 'undefined' && typeof path !== 'undefined') {
     return require(path); // running on node.js
   }
 }
